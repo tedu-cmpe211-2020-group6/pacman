37fa25f714244a9e531a460bbe0569a52a6621eb
begumpls <58304276+begumpls@users.noreply.github.com>
2020-12-19 21:31:21 +0300

    Add files via upload

---
+++ GHOSTS.java
@@ -0,0 +1,18 @@
+package Pacman;
+
+public class GHOSTS {
+private int numberofGhosts=6;
+private final int MaxiumumGhosts=12;
+public int getMaxiumumGhosts() {
+	return MaxiumumGhosts;
+}
+public int getNumberofGhosts() {
+	return numberofGhosts;
+}
+public void setNumberofGhosts(int numberofGhosts) {
+	this.numberofGhosts = numberofGhosts;
+}
+
+
+
+}
